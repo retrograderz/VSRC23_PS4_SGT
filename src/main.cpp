d9d02d4bec903362c395e8259d9565868a190189
RETROGRADERZ <hereda.unknown@gmail.com>
2023-09-17 18:40:25 +0700

    1st. include servo - no run

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,29 @@ int8_t LY, Rx;
 DCMotor VSRC_Motor;
 Servo_Motor VSRC_Servo;
 
+
 void setup() {   
     Serial.begin(115200);
     VSRC_Motor.Init();
     VSRC_Servo.Init();
     PS4.begin("1a:3b:5c:7d:9e:ff");
     Serial.println("READY");
+
+    VSRC_Servo.Pulse(INTAKE_PULSE_0, INTAKE_SERVO);
+    VSRC_Servo.Pulse(SHOOTER_PULSE_0, SHOOTER);
+    VSRC_Servo.Pulse(LIFT_PULSE_0, LIFT);
+
 }
+
+static bool status_INTAKE = 0;
+static bool status_SHOOTER = 0;
+static bool status_LIFT = 0;
 
 void loop() {
     if (PS4.isConnected()) {
         LY = PS4.LStickY();
         Rx = PS4.RStickX();
+        
         // CALC PWM
             if ((LY > -30 && LY < 30) && (Rx > -30 && Rx < 30)) {
                 pwm_left = 0;
@@ -48,14 +59,55 @@ void loop() {
                 pwm_left = LIMIT_PWM;
                 pwm_right = LIMIT_PWM;
             }
+
+        // intake - L1
+        if (PS4.L1() == 1) {
+            if (status_INTAKE == 0) {
+                VSRC_Servo.Pulse(INTAKE_PULSE_1, INTAKE_SERVO);
+            } else {
+                VSRC_Servo.Pulse(INTAKE_PULSE_0, INTAKE_SERVO);
+            } 
+            status_INTAKE = !status_INTAKE;
+        }
+        
+        // shooter - R1
+        if (PS4.R1() == 1) {
+            if (status_SHOOTER == 0) {
+                VSRC_Servo.Pulse(SHOOTER_PULSE_1, SHOOTER);
+            } else {
+                VSRC_Servo.Pulse(SHOOTER_PULSE_0, SHOOTER);
+            } 
+            status_SHOOTER = !status_SHOOTER;
+        }
+
+        // lift - SQUARE
+        if (PS4.Square() == 1) {
+            // if (status_LIFT == 0) {
+                VSRC_Servo.Pulse(LIFT_PULSE_1, LIFT);
+                Serial.println("sqr 1");
+        // }
+        }
+
+        // lift - SQUARE
+        if (PS4.Circle() == 1) {
+            // if (status_LIFT == 0) {
+                VSRC_Servo.Pulse(LIFT_PULSE_0, LIFT);
+                Serial.println("sqr 0");
+        // }
+        }
     }
+            // } else {
+            //     VSRC_Servo.Pulse(LIFT_PULSE_0, LIFT);
+            //     Serial.println("sqr 0");
+            // } 
+            // status_LIFT = !status_LIFT;
     
     if (pwm_left > LIMIT_PWM) pwm_left = LIMIT_PWM;
     if (pwm_left < 0) pwm_left = 0;
     if (pwm_right > LIMIT_PWM) pwm_right = LIMIT_PWM;
     if (pwm_right < 0) pwm_right = 0;
  
-    Serial.printf("pwm L: %d pwm R: %d \n", pwm_left, pwm_right);
+    // Serial.printf("pwm L: %d pwm R: %d \n", pwm_left, pwm_right);
     VSRC_Motor.Run(LEFT_MOTOR, pwm_left, dir_left);
     VSRC_Motor.Run(RIGHT_MOTOR, pwm_right, dir_right);
     delay(5);
--- src/main.h
+++ src/main.h
@@ -8,12 +8,25 @@
 #define LEFT_MOTOR 2
 #define RIGHT_MOTOR 3
 
+#define INTAKE_SERVO 1
+#define SHOOTER 2
+#define LIFT 3 
+
+#define INTAKE_PULSE_1 1600
+#define INTAKE_PULSE_0 1000
+
+#define SHOOTER_PULSE_1 1700
+#define SHOOTER_PULSE_0 1000
+
+#define LIFT_PULSE_1 2000
+#define LIFT_PULSE_0 1000
+
 #define SPR(a) Serial.print(a);
 #define SPRL(a) Serial.println(a);
 
-// struct entrypoints {
-//     uint8_t lower_bound
-
-// } EP;
+struct entry_points {
+    uint8_t low_forw_LY, high_forw_LY, low_back_LY, high_back_LY;
+    uint8_t low_right_RX, high_right_RX, low_left_RX, high_left_RX;
+} EP;
 
 #endif
